a00b38a318fd928572cef0a6b97b9e8f10e20497
Sergey Pavlov <pavlovsv93@gmail.com>
2022-01-14 20:23:08 +0300

    Add new View

---
+++ app/src/main/java/com/gmail/pavlovsv93/whattoseetoday/view/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.gmail.pavlovsv93.whattoseetoday.view
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/gmail/pavlovsv93/whattoseetoday/view/WhatToSeeActivity.kt
@@ -0,0 +1,14 @@
+package com.gmail.pavlovsv93.whattoseetoday.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gmail.pavlovsv93.whattoseetoday.R
+
+class WhatToSeeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_what_to_see)
+
+
+    }
+}
